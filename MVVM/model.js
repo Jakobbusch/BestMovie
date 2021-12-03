4beac0465402b99739189417c5b5258109492dec
Jakob Busch Hansen <jakob-busch@hotmail.com>
2021-12-03 11:10:59 +0100

    asd

---
+++ MVVM/model.js
@@ -0,0 +1,12 @@
+const model = (movies, filter =() => true) => {
+    const moviesMap = {}
+    movies.forEach(m => moviesMap[""] = w)
+
+    
+
+    return {movies}
+    
+    
+}
+
+export default model
---
+++ MVVM/viewmodel.js
@@ -0,0 +1,8 @@
+export default (el, init_model) => {
+    let model = init_model
+    return{
+
+
+
+    }    
+}

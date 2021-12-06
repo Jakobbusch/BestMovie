d82c06dbc744ed361abc6096b28b96c19682bb75
Irongarden <mathiashave@me.com>
2021-12-06 12:08:29 +0100

    MVVM structure works

---
+++ assets/ViewModel.js
@@ -0,0 +1,25 @@
+import {ajax} from 'https://dev.jspm.io/rxjs@6/_esm2015/ajax'
+import {interval, of, merge} from 'https://dev.jspm.io/rxjs@6/_esm2015'
+import {filter, map, concatMap,share,mergeScan} from 'https://dev.jspm.io/rxjs@6/_esm2015/operators'
+
+
+export default (el, init_model) => {
+    let model = init_model
+
+return {
+    el,
+    data:{
+        movieTitle:'',
+        movie: model.movie
+    },
+    
+    
+    
+    methods:{
+        async writeToConsole(){
+                console.log("Hello my friend")
+        }
+    }
+    
+}
+}

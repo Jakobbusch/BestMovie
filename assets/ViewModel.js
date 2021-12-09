b1316562ffc61b581f25c7a24cd21fdf241bc2e4
Irongarden <mathiashave@me.com>
2021-12-09 11:33:23 +0100

    loginButton No work

--- assets/ViewModel.js
+++ assets/ViewModel.js
@@ -1,7 +1,3 @@
-import {ajax} from 'https://dev.jspm.io/rxjs@6/_esm2015/ajax'
-import {interval, of, merge} from 'https://dev.jspm.io/rxjs@6/_esm2015'
-import {filter, map, concatMap,share,mergeScan} from 'https://dev.jspm.io/rxjs@6/_esm2015/operators'
-
 
 export default (el, init_model) => {
     let model = init_model
@@ -26,6 +22,7 @@ return {
     },
     async search(){
         if(this.movieTitle!=''){
+            console.log("hello")
             const movie_res = await fetch('https://www.omdbapi.com/?apikey=8ea3b105&t='+this.movieTitle).then(res => res.json())
             this.movie = movie_res
             //console.log(this.movie.Released)
@@ -40,22 +37,6 @@ return {
         if(this.movie.Title!=undefined){
             console.log(this.movie.Title + ' Added to favourites')
         }
-
-        var con = import ('mysql');
-  var con = createPool({
-     host: "34.159.166.233",
-    user: "root",
-    password: "k4j4mnzswek",
-    database: "MyDB",
-    port: 3306
-    });
-
-    con.connect(function(err) {
-      if (err) throw err;
-      console.log("Connected!");
-    });
-
-        
         
     }
 

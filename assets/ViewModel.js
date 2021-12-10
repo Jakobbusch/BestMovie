08baf667c9ec1e1255a566bfc2aa77ef3a555590
Jakob Busch Hansen <jakob-busch@hotmail.com>
2021-12-10 11:43:53 +0100

    Move firebase to Viewmodel

--- assets/ViewModel.js
+++ assets/ViewModel.js
@@ -1,6 +1,59 @@
+import { initializeApp } from "https://www.gstatic.com/firebasejs/9.6.0/firebase-app.js";
+import { getAuth, signInWithRedirect , GoogleAuthProvider, onAuthStateChanged ,signOut  } from "https://www.gstatic.com/firebasejs/9.6.0/firebase-auth.js";
+
 
 export default (el, init_model) => {
     let model = init_model
+
+
+    const firebaseConfig = {
+        apiKey: "AIzaSyBjMvVKgFVEr2XHD8t3Ot5ukXFO9W4MUDk",
+        authDomain: "semester6project.firebaseapp.com",
+        projectId: "semester6project",
+        storageBucket: "semester6project.appspot.com",
+        messagingSenderId: "12632593485",
+        appId: "1:12632593485:web:a9ee8e9b6ec0faa701b804"
+      };
+
+    const app = initializeApp(firebaseConfig);
+
+    const auth = getAuth();
+
+        auth.languageCode = 'it';
+
+    const provider = new GoogleAuthProvider();
+
+        provider.addScope('https://www.googleapis.com/auth/contacts.readonly');
+        provider.setCustomParameters({
+            'login_hint': 'xxx@gmail.com'
+        });
+
+    const user = auth.currentUser;
+
+    onAuthStateChanged(auth, (user) => {
+    if (user) {
+        // User is signed in, see docs for a list of available properties
+        // https://firebase.google.com/docs/reference/js/firebase.User
+        const uid = user.uid;
+        
+        if (user !== null) {
+        user.providerData.forEach((profile) => {
+        document.getElementById('loginHide').style.visibility = 'visible';
+        console.log("Sign-in provider: " + profile.providerId);
+        console.log("  Provider-specific UID: " + profile.uid);
+        console.log("  Name: " + profile.displayName);
+        console.log("  Email: " + profile.email);
+        console.log("  Photo URL: " + profile.photoURL);
+        console.log("{{maybe}}");
+    });
+    }
+    } else {
+        console.log("Login is NOT Working!")
+        document.getElementById('loginHide').style.visibility = 'hidden';
+    }
+
+
+});
 
 return {
     el,
@@ -44,6 +97,19 @@ return {
         console.log(res)
         
         
+    },
+    
+    async loginGoogle() {
+        console.log("Hello from google")
+        signInWithRedirect(auth, provider); 
+      },
+
+    async logout() {
+        signOut(auth).then(() => {
+       console.log("You have been signed out")
+    }).catch((error) => {
+      console.log("Error")
+        });
     }
 
         
